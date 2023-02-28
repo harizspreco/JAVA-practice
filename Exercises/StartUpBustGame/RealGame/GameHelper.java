43d45bcc27217233b35d1447badda3d0323d1ce8
harizspreco <hariz.business32@gmail.com>
2023-02-28 23:51:14 +0100

    GameHelper class setup

--- Exercises/StartUpBustGame/RealGame/GameHelper.java
+++ Exercises/StartUpBustGame/RealGame/GameHelper.java
@@ -1,9 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class GameHelper{
+	private static final String ALPHABET = "abcdefg";
+	private static final int GRID_LENGTH = 7;
+	private static final int GRID_SIZE = 49;
+	private static final int MAX_ATTEMPTS = 200;
+	static final int HORIZONTAL_INCREMENT = 1;
+	static final int VERTICAL_INCREMENT = GRID_LENGTH;
+
+	private final int[] grid = new int(GRID_SIZE);
+	private final Random random = new Random();
+	private int startupCount = 0;
+
 	public int getUserInput(String prompt){
-		System.out.println(prompt + ": ");
-		Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+		
 	}
 }
