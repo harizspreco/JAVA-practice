1c7503a932062487933f792eed0d5fdbeac53a3c
harizspreco <hariz.business32@gmail.com>
2023-03-15 16:36:28 +0100

    Generics, sorting, lambda expressions - practice

--- Exercises/5/Jukebox.java
+++ Exercises/5/Jukebox.java
@@ -3,19 +3,40 @@ import java.util.*;
 public class Jukebox {
 	public static void main(String[] args){
 		new Jukebox().go();
+		new Jukebox().goWithLabmda();
 	}
 
 	public void go(){
 		List<SongV2> songList = MockSongs.getSongsV2();
-		Collections.sort(songList);
+		//Collections.sort(songList);
+		TitleComparator titleComparator = new TitleComparator();
+		Collections.sort(songList,titleComparator); // or songList.sort(titleComparator); 
 		System.out.println(songList);
+
+		ArtistComparator artistComparator = new ArtistComparator();
+		Collections.sort(songList,artistComparator);
+		System.out.println(songList);
+	}
+
+	public void goWithLabmda(){
+		List<SongV2> songList = MockSongs.getSongsV2();
+		songList.sort((one,two) -> one.getTitle().compareTo(two.getTitle()));
+		System.out.println("Sorted with lambda, by title" + songList);
+
+		//sort by BPM
+		songList.sort((one,two) -> Integer.compare(one.getBpm(), two.getBpm()));
+		System.out.println("Sorted with lambda, by BPM" + songList);
+
+		//sort by title in descending order
+		songList.sort((one,two) -> two.getTitle().compareTo(one.getTitle()));
+		System.out.println("Sorted with lambda, by title, descending" + songList);
 	}
 }
 
 //we need to "extend/implement" Comparable in order to be able to sort SongV2 objects
 //Comparable have compareTo() method that we need to implement in our class
 //We will implement it so it can sort song objects by title.
-class SongV2 implements Comparable<SongV2>, Comparator<SongV2>{
+class SongV2 implements Comparable<SongV2>{
 	private String title;
 	private String artist;
 	private int bpm;
@@ -45,6 +66,18 @@ class SongV2 implements Comparable<SongV2>, Comparator<SongV2>{
 
 	public String toString(){
 		return title;
+	}
+}
+
+class ArtistComparator implements Comparator<SongV2>{
+	public int compare(SongV2 o1, SongV2 o2){
+		return o1.getArtist().compareTo(o2.getArtist());
+	}
+}
+
+class TitleComparator implements Comparator<SongV2>{
+	public int compare(SongV2 o1, SongV2 o2){
+		return o1.getTitle().compareTo(o2.getTitle());
 	}
 }
 
